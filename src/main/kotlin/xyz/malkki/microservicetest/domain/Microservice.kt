e0839694594e34070d2ff731c602fbf21d534ad8
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-06-08 15:42:42 +0300

    Add support for configuring wait strategy for containers

--- src/main/kotlin/xyz/malkki/microservicetest/domain/Microservice.kt
+++ src/main/kotlin/xyz/malkki/microservicetest/domain/Microservice.kt
@@ -1,6 +1,7 @@
 package xyz.malkki.microservicetest.domain
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import java.time.Duration
 
 data class Microservice(val id: String,
@@ -8,6 +9,7 @@ data class Microservice(val id: String,
                         val ports: List<Int>,
                         val cmd: String?,
                         val startupTimeout: Duration?,
+                        val waitStrategy: WaitStrategy?,
                         val environment: Map<String, Any> = emptyMap(),
                         val dependencies: List<String> = emptyList()
 ) {
@@ -17,6 +19,15 @@ data class Microservice(val id: String,
         } else {
             GenericContainer(container)
         }
+
+        if (waitStrategy != null) {
+            container.setWaitStrategy(when (waitStrategy.type) {
+                WaitStrategy.Type.PORT -> Wait.forListeningPort()
+                WaitStrategy.Type.HEALTHCHECK -> Wait.forHealthcheck()
+                WaitStrategy.Type.LOG -> Wait.forLogMessage(waitStrategy.logMessage!!, 1)
+            })
+        }
+
         container.networkAliases = listOf(id)
         container.exposedPorts = ports
         cmd?.let { container.setCommand(it) }
@@ -24,5 +35,21 @@ data class Microservice(val id: String,
         container.env = environment.map { (key, value) -> "$key=$value" }
 
         return container
+    }
+
+    data class WaitStrategy(val type: Type, val logMessage: String?) {
+        enum class Type {
+            PORT, LOG, HEALTHCHECK;
+
+            companion object {
+                fun valueOfSafe(value: String): Type? {
+                    return try {
+                        Type.valueOf(value)
+                    } catch (iae: IllegalArgumentException) {
+                        null
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/xyz/malkki/microservicetest/testdefinition/MicroserviceConfigParser.kt
+++ src/main/kotlin/xyz/malkki/microservicetest/testdefinition/MicroserviceConfigParser.kt
@@ -23,10 +23,24 @@ class MicroserviceConfigParser {
 
         val startupTimeout = serviceConfig["startupTimeout"]?.let { it as Int }?.let { Duration.ofSeconds(it.toLong()) }
 
+        val waitStrategyConfig = serviceConfig["waitStrategy"]?.let { it as Map<String, Any> }
+        waitStrategyConfig?.checkKey("type")
+
+        val waitStrategyType = Microservice.WaitStrategy.Type.valueOfSafe(waitStrategyConfig?.get("type").toString())
+        val waitStrategy = if (waitStrategyType == Microservice.WaitStrategy.Type.LOG) {
+            waitStrategyConfig!!.checkKey("logMessage")
+
+            Microservice.WaitStrategy(waitStrategyType, waitStrategyConfig["logMessage"]!!.toString())
+        } else if (waitStrategyType != null) {
+            Microservice.WaitStrategy(waitStrategyType, null)
+        } else {
+            null
+        }
+
         val environment: Map<String, Any> = serviceConfig["environment"]?.let { it as Map<String, Any> } ?: emptyMap()
 
         val dependencies = serviceConfig["dependencies"]?.let { it as List<String> }.orEmpty()
 
-        return Microservice(serviceConfig["id"]!!.toString(), serviceConfig["container"]!!.toString(), exposedPorts, serviceConfig["cmd"]?.toString(), startupTimeout, environment, dependencies)
+        return Microservice(serviceConfig["id"]!!.toString(), serviceConfig["container"]!!.toString(), exposedPorts, serviceConfig["cmd"]?.toString(), startupTimeout, waitStrategy, environment, dependencies)
     }
 }
