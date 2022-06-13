b9a8494e70fe1090ba141acdfb2a648028a0fbc9
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-06-13 15:49:24 +0300

    Use concurrent hash map for test state

--- src/main/kotlin/xyz/malkki/microservicetest/testexecution/TestStepExecutor.kt
+++ src/main/kotlin/xyz/malkki/microservicetest/testexecution/TestStepExecutor.kt
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.assertTimeout
 import org.testcontainers.containers.GenericContainer
 import xyz.malkki.microservicetest.domain.TestStep
 import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.full.createInstance
 
 class TestStepExecutor(private val containers: Map<String, GenericContainer<*>>) {
     fun executeSteps(steps: List<TestStep>) {
-        val state = mutableMapOf<String, Any>()
+        val state = ConcurrentHashMap<String, Any>()
 
         steps.forEach { step ->
             //TODO: check if class exists
