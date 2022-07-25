93c1eb3808419e995e59a24ffdea7f073ed96ec0
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-07-25 14:20:54 +0300

    Add @Throws annotation to TestStepCode interface for better Java interop

--- src/main/kotlin/xyz/malkki/microservicetest/testexecution/TestStepCode.kt
+++ src/main/kotlin/xyz/malkki/microservicetest/testexecution/TestStepCode.kt
@@ -8,5 +8,6 @@ interface TestStepCode {
      * @param containers Map of containers that have been defined as a dependency for the test step
      * @param state Mutable map of test state
      */
+    @Throws(Exception::class)
     fun execute(containers: Map<String, GenericContainer<*>>, updateState: (key: String, updater: (Any?) -> Any) -> Unit, getState: (key: String) -> Any?)
 }
